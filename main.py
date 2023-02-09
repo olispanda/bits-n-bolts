af487fa6a99ba4fbe02babcc62854601f47f4609
olispanda <91331850+olispanda@users.noreply.github.com>
2023-02-09 11:43:35 +0100

    Fix: Video does Loop and load
    Problem: The Video open itself more and more (everything starts lagging)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 const = 3
 while (const == 3):
-    from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
     import pygame
+    from moviepy.editor import *
 
     pygame.display.set_mode((0, 0), 0, 32)
-    clip = VideoFileClip('/bitsnbolts/videos/test.mp4').resize((800, 480))
+    clip = VideoFileClip('videos/test.mp4').resize((1920, 1080))
     clip.preview()
+    pygame.quit()
 
-    if (event.type == pygame.KEYDOWN):
-        if (event.type == pygame.K_e):
-            break
+    # if (event.type == pygame.KEYDOWN):
+    #     if (event.type == pygame.K_e):
+    #         break
