fecf0bae89d74072e6201747b409b9fa500189fd
olispanda <91331850+olispanda@users.noreply.github.com>
2023-02-26 16:09:00 +0100

    wip: trying to play a video and check if Button is pressed in multible threads

--- main.py
+++ main.py
@@ -1,59 +1,29 @@
-# import thread
-from multiprocessing.connection import wait
-import RPi.GPIO as GPIO
-import time
 from time import sleep
-from subprocess import Popen, PIPE
-import pexpect
-
-
+from moviepy.editor import *
+import threading
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 
-# VIDEO_CLIP_IDLE = "videos/test-720.mp4"
-# video_player = Popen(['vlc', '--fullscreen', '--loop',
-#                       '--no-video-title-show', '--no-audio', '--quiet', VIDEO_CLIP_IDLE])
 
-# video_player = Popen(['vlc', '--intf', 'rc'], stdin=PIPE)
-# video_player.stdin.write('help')
-
-videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
-videoplayer.sendline('add videos/test-720.mp4')
-# videoplayer.sendline('get_length')
-videoplayer.sendline('play')
+def loop():
+    while True:
+        pass
+        # loop vid
 
 
-print("yolo")
+def button():
+    while True:
+        print("checking")
+        if GPIO.input(24) == 1:
+            GPIO.output(23, GPIO.HIGH)
+        else:
+            pass
 
-buttonPressed = False
 
-while True:
-    # if button off
-    if GPIO.input(24) == 0:
-        GPIO.output(23, GPIO.LOW)
-        buttonPressed = False
-    # if button on
-    else:
-        GPIO.output(23, GPIO.HIGH)
-        if buttonPressed == False:
-            print('xolo2')
-            videoplayer.sendline('pause')
-            buttonPressed = True
-            if buttonPressed == True:
-                videoplayer.sendline('add videos/test2.mp4')
-                sleep(10)
-                print('yolo3')
-                # break
-
-        # # import pygame
-        # # from RPi import GPIO
-
-        # video_player = Popen(['vlc', '--fullscreen', '--loop',
-        #                      '--no-video-title-show', '--no-audio', '--quiet', VIDEO_CLIP_IDLE])
-        # while True:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.KEYDOWN:
-        #             if event.unicode == 'x':
-        #                 print('du hast die richtige taste gedrückt')
+global x
+x = threading.Thread(target=loop)
+x.start()
+y = threading.Thread(target=button)
+y.start()
