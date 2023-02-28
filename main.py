097799c27ef50f458af7f0d5b848e083858be916
olispanda <91331850+olispanda@users.noreply.github.com>
2023-02-28 10:14:08 +0100

    Revert "wip: Video looping"
    This reverts commit b4671d2a9c70903b3d74b82c3a5474697117d6b5.

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ GPIO.setup(24, GPIO.IN)
 
 videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
 videoplayer.sendline('add videos/test-720.mp4')
-# videoplayer.sendline('get_length')
 videoplayer.sendline('play')
 
 
@@ -45,7 +44,7 @@ while True:
                 videoplayer.sendline('add videos/test2.mp4')
                 sleep(10)
                 print('yolo3')
-                # break
+                break
 
         # # import pygame
         # # from RPi import GPIO
