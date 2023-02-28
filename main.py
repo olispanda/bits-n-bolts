4c16cf96f1b79bc75197fb978435b34ca998a3ed
olispanda <91331850+olispanda@users.noreply.github.com>
2023-02-28 22:21:40 +0100

    wip: testing button
    Just dosent work right now and idk why

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 
-buttonPressed = False
 videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
+videoplayer.sendline('add videos/test-720.mp4')
+videoplayer.sendline('play')
+print("yolo")
+
+buttonPressed = False
+
+videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show')
 videoplayer.sendline('add videos/test-720.mp4')
 videoplayer.sendline('play')
 print("yolo")
@@ -28,5 +34,3 @@ while True:
             videoplayer.sendline('add videos/test2.mp4')
             sleep(10)
             print('yolo3')
-            buttonPressed == False
-            videoplayer.sendline('play')
