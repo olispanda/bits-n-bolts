58bca6f32287b7a4fc4eb94884b5344ed36518a8
olispanda <91331850+olispanda@users.noreply.github.com>
2023-02-28 22:05:10 +0100

    wip: trying to fix the loop

--- main.py
+++ main.py
@@ -11,29 +11,38 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 
+buttonPressed = False
 
-videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
-videoplayer.sendline('add videos/test-720.mp4')
-videoplayer.sendline('play')
+# _______________________________________________________________________
 
+videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show')
+
+
+def Video1():
+    videoplayer.sendline('add videos/test-720.mp4 --loop')
+    videoplayer.sendline('play')
+
+
+Video1
+
+# ________________________________________________________________________
 
 print("yolo")
-buttonPressed = False
+
 
 while True:
     if GPIO.input(24) == 0:
         GPIO.output(23, GPIO.LOW)
         buttonPressed = False
-        Video1
     else:
         GPIO.output(23, GPIO.HIGH)
         if buttonPressed == False:
             print('xolo2')
             videoplayer.sendline('pause')
             buttonPressed = True
-
-            if buttonPressed == True:
-                videoplayer.sendline('add videos/test2.mp4')
-                sleep(10)
-                print('yolo3')
-                break
+        elif buttonPressed == True:
+            videoplayer.sendline('add videos/test2.mp4')
+            sleep(10)
+            print('yolo3')
+            buttonPressed == False
+            Video1
