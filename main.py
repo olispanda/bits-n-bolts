1b86b9aa2b90ed2ba96bf00160cea7fa25e33ea3
olispanda <91331850+olispanda@users.noreply.github.com>
2023-03-02 22:14:17 +0100

    wip: no loop in second video

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 
-videoplayer = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
-videoplayer.sendline('play videos/test-720.mp4')
+videoplayer1 = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
+videoplayer1.sendline('play videos/test-720.mp4')
+
 
 print("yolo")
 buttonPressed = False
@@ -22,13 +23,14 @@ while True:
     if GPIO.input(24) == 0:
         GPIO.output(23, GPIO.LOW)
         buttonPressed = False
-        print('yolo4')
 
     # if button is pressed:
     else:
         GPIO.output(23, GPIO.HIGH)
         if buttonPressed == False:
             print('xolo2')
-            videoplayer.sendline('pause')
-            videoplayer.sendline('add videos/test2.mp4')
+            videoplayer1.sendline('pause')
+            videoplayer2 = pexpect.spawn('vlc --intf rc --no-video-title-show')
+            videoplayer2.sendline('play videos/test2.mp4')
+            videoplayer1.sendline('play')
             print('yolo3')
