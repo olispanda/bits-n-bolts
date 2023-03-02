0a7bc64683a01e06f17d08ac0557a416576e1414
olispanda <91331850+olispanda@users.noreply.github.com>
2023-03-02 22:26:52 +0100

    wip: still trying to fix same issue :(

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.IN)
 
 videoplayer1 = pexpect.spawn('vlc --intf rc --no-video-title-show --loop')
-videoplayer1.sendline('play videos/test-720.mp4')
+videoplayer1.sendline('add videos/test-720.mp4')
+videoplayer1.sendline('play')
 
 
 print("yolo")
@@ -31,6 +32,6 @@ while True:
             print('xolo2')
             videoplayer1.sendline('pause')
             videoplayer2 = pexpect.spawn('vlc --intf rc --no-video-title-show')
-            videoplayer2.sendline('play videos/test2.mp4')
+            videoplayer2.sendline('add videos/test-720.mp4')
             videoplayer1.sendline('play')
             print('yolo3')
