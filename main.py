fcd00c209ff352b74413dd041ed1db68e22f5238
olispanda <91331850+olispanda@users.noreply.github.com>
2023-03-03 14:55:05 +0100

    wip:testing if smaller videos work better

--- main.py
+++ main.py
@@ -30,17 +30,22 @@ while True:
     else:
         GPIO.output(23, GPIO.HIGH)
         if buttonPressed == False:
-            print('xolo2')
-            videoplayer1.sendline('quit')
-            videoplayer1.close()
-            videoplayer2 = pexpect.spawn(
-                'vlc --no-video-title-show bits-n-bolts/videos/video2.mp4')
-            videoplayer2.sendline('add videos/video2.mp4')
-            sleep(29)
-            videoplayer2.sendline('quit')
-            videoplayer2.close()
-            videoplayer1 = pexpect.spawn(
-                'vlc --no-video-title-show --loop bits-n-bolts/videos/video1.mp4')
-            videoplayer1.sendline('add videos/video1.mp4')
-            videoplayer1.sendline('play')
-            print('yolo3')
+            try:
+                print('xolo2')
+                videoplayer1.sendline('quit')
+                videoplayer1.close()
+                videoplayer2 = pexpect.spawn(
+                    'vlc --no-video-title-show bits-n-bolts/videos/video2.mp4')
+                videoplayer2.sendline('add videos/video2.mp4')
+                sleep(29)
+                videoplayer2.sendline('quit')
+                videoplayer2.close()
+                videoplayer1 = pexpect.spawn(
+                    'vlc --no-video-title-show --loop bits-n-bolts/videos/video1.mp4')
+                videoplayer1.sendline('add videos/video1.mp4')
+                videoplayer1.sendline('play')
+                print('yolo3')
+            except:
+                print("try catch failed")
+            finally:
+                print("End of try catch: finally")
